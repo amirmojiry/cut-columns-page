1819d60fe22260b22713fb11e9158d7bbb4255d8
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2019-10-05 08:38:09 +0330

    edit merge_pages: global change first and end pages

--- merge_pages.php
+++ merge_pages.php
@@ -3,10 +3,14 @@
 ini_set('max_execution_time', 400000000000);
 include_once 'functions.php';
 
-for ($i = 1405; $i <= 1541; $i+=4) {
-    $i = strval($i);
-    $j = strval($i + 2);
-    merge_two_image ("merged_output2/".$i.".jpg", "merged_output2/".$j.".jpg");
+if (! empty ( $_GET['first'] ) && ! empty ( $_GET['end'] ) ) {
+    $first = $_GET['first'];
+    $end = $_GET['end'];
+    for ($i = $first; $i <= $end; $i++) {
+        $i = strval($i);
+        $j = strval($i + 2);
+        merge_two_image ("merged_output2/".$i.".jpg", "merged_output2/".$j.".jpg");
+    }
 }
 
 ?>
